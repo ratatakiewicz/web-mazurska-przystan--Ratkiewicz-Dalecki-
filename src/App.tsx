a34b370796e8c8fcd974bb6e2a8e55476f855ec4
Jakub <kubix091027@gmail.com>
2026-03-14 22:51:48 +0100

    dodano stala Calculator oraz naglowek strony

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,12 @@
+
 import './App.css';
+
+const Calculator = () => {
+    return (
+        <div className="app-container">
+            <div className="calc-card">
+                <header className="calc-header">
+                    <h2>⚓ Mazurska Przystań 2026</h2>
+                    <p>Kalkulator rezerwacji sprzętu</p>
+                </header></div>
+
